3a956e9307de962c5ca5420b3584980c64bcab60
Oriya124578 <oriya.margalit@post.runi.ac.il>
2025-10-31 00:37:10 +0200

    homework 2

--- FVCalc.java
+++ FVCalc.java
@@ -2,8 +2,21 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
-		int a = 5;
-		int b = 10;
+
+		int currentvalue = Integer.parseInt(args[0]);
+
+		double ratePercent = Double.parseDouble(args[1]);
+		double rate = ratePercent / 100.0;
+
+		int years = Integer.parseInt(args[2]);
+		double n = (double) years;
+
+		double futurevalue = currentvalue * Math.pow((1 + rate), n);
+
+		int futurevalueint = (int) futurevalue; 
+
+		System.out.println("After " + years + " years, " + "$" + currentvalue + " saved at " + ratePercent + "% will yield $" + futurevalueint );
+
 
 		
 	}
