268228725308ccde5b93c8e44cd843bf4c90fc0c
Oriya124578 <oriya.margalit@post.runi.ac.il>
2025-10-30 23:10:17 +0200

    Completed Bill3 program for Homework 1

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,14 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
+		String name2 = args[1];
+		String name3 = args[2];
+		int totalbill = Integer.parseInt(args[3]);
+
+		double finalPayment = Math.ceil((double) totalbill / 3.0);
+
+		System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + finalPayment + " Shekels each.");
+
 	    // Replace this comment with the rest of your code   
 	}
 }
